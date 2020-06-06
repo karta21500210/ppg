1dae4dcc29073313670574f1108e490e9be5334a
karta21500210 <karta21500210@yahoo.com.tw>
2020-06-06 16:38:52 +0800

    較正常版

--- sketch_apr17a2.ino
+++ sketch_apr17a2.ino
@@ -5,15 +5,15 @@
 #define output_length 5
 uint16_t irbuffer[buffer_length], redbuffer[buffer_length], iractop[output_length], redactop[output_length], iracbottom[output_length], redacbottom[output_length];
 int8_t irbuffer_d[buffer_length], redbuffer_d[buffer_length];
-uint8_t flag = 0, ir_diff_treth=10, red_diff_treth, heart_t_treth=30,topbuffer[output_length], bottombuffer[output_length], sibuffer[output_length];
+uint8_t flag = 0, ir_diff_treth = 10, red_diff_treth, heart_t_treth = 30, topbuffer[output_length], bottombuffer[output_length], sibuffer[output_length];
 uint8_t output_flag = 0;
-uint32_t a = 0, time_t1=0, top_temp; 
+uint32_t a = 0, time_t1 = 0, top_temp;
 MAX30105 particleSensor; // initialize MAX30102 with I2C
 
 void setup()
 {
   Serial.begin(115200);
-  while(!Serial); //We must wait for Teensy to come online
+  while (!Serial); //We must wait for Teensy to come online
   delay(100);
   Serial.println("");
   Serial.println("MAX30102");
@@ -34,32 +34,25 @@ void setup()
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
   Serial.println("IR,RED,ir_d,red_d");
-}
-
-
-void loop() {
+  //先填滿BUFFER
   int16_t temp;
-  particleSensor.check(); //Check the sensor
-  while (particleSensor.available()) {
-      // read stored IR
-      //Serial.print(micros());
-      //Serial.print(",");
-//      a =  millis() - a;
-//      Serial.print("AAAAA:");
-//      Serial.print(a);
-//      a =  millis(); 
-//取得波型
+  bool ok= 1 ;
+  while (ok)
+  {
+    particleSensor.check();
+    while (particleSensor.available())
+    {
       irbuffer[flag] = - uint16_t(particleSensor.getIR());
       redbuffer[flag] = - uint16_t(particleSensor.getRed());
-//一次微分
-      temp = int16_t((irbuffer[flag] - irbuffer[flag-1>=0 ? flag-1 : buffer_length-1])*5);
-//      Serial.print(temp);
-//      Serial.print(",");
-      if(temp >= 127)
+      //一次微分
+      temp = int16_t((irbuffer[flag] - irbuffer[flag - 1 >= 0 ? flag - 1 : buffer_length - 1]) * 5);
+      //      Serial.print(temp);
+      //      Serial.print(",");
+      if (temp >= 127)
       {
         irbuffer_d[flag] = 127;
       }
-      else if(temp <= -128)
+      else if (temp <= -128)
       {
         irbuffer_d[flag] = -128;
       }
@@ -67,13 +60,13 @@ void loop() {
       {
         irbuffer_d[flag] = int8_t(temp);
       }
-      temp = int16_t((redbuffer[flag] - redbuffer[flag-1>=0 ? flag-1 : buffer_length-1])*5);
-//      Serial.print(temp);
-      if(temp >= 127)
+      temp = int16_t((redbuffer[flag] - redbuffer[flag - 1 >= 0 ? flag - 1 : buffer_length - 1]) * 5);
+      //      Serial.print(temp);
+      if (temp >= 127)
       {
         redbuffer_d[flag] = 127;
       }
-      else if(temp <= -128)
+      else if (temp <= -128)
       {
         redbuffer_d[flag] = -128;
       }
@@ -81,58 +74,116 @@ void loop() {
       {
         redbuffer_d[flag] = int8_t(temp);
       }
-      
-//      redbuffer_d[flag] = int8_t(float(redbuffer[flag] - redbuffer[flag-1>=0 ? flag-1 : buffer_length-1])*3);
-//      Serial.print(",");
-      Serial.print(irbuffer_d[flag]);
-      Serial.print(",");
-////      // read stored red
-      Serial.print(redbuffer_d[flag]);
-      Serial.print(",");
+      flag++;
+      if (flag >= buffer_length)
+      {
+        flag = 0;
+        ok = 0;
+        break;
+      }
+    }
+  }
+}
 
-//波峰
-      if(irbuffer_d[flag-1>=0 ? flag-1 : buffer_length-1] >= 0 and irbuffer_d[flag] < 0 and (irbuffer_d[flag-4>=0 ? flag-4 : buffer_length-4] >= ir_diff_treth*0.7))
+
+void loop() {
+  int16_t temp;
+  particleSensor.check(); //Check the sensor
+  while (particleSensor.available()) {
+    // read stored IR
+    //Serial.print(micros());
+    //Serial.print(",");
+    //      a =  millis() - a;
+    //      Serial.print("AAAAA:");
+    //      Serial.print(a);
+    //      a =  millis();
+    //取得波型
+    irbuffer[flag] = - uint16_t(particleSensor.getIR());
+    redbuffer[flag] = - uint16_t(particleSensor.getRed());
+    //一次微分
+    temp = int16_t((irbuffer[flag] - irbuffer[flag - 1 >= 0 ? flag - 1 : buffer_length - 1]) * 5);
+    //      Serial.print(temp);
+    //      Serial.print(",");
+    if (temp >= 127)
+    {
+      irbuffer_d[flag] = 127;
+    }
+    else if (temp <= -128)
+    {
+      irbuffer_d[flag] = -128;
+    }
+    else
+    {
+      irbuffer_d[flag] = int8_t(temp);
+    }
+    temp = int16_t((redbuffer[flag] - redbuffer[flag - 1 >= 0 ? flag - 1 : buffer_length - 1]) * 5);
+    //      Serial.print(temp);
+    if (temp >= 127)
+    {
+      redbuffer_d[flag] = 127;
+    }
+    else if (temp <= -128)
+    {
+      redbuffer_d[flag] = -128;
+    }
+    else
+    {
+      redbuffer_d[flag] = int8_t(temp);
+    }
+
+    //      redbuffer_d[flag] = int8_t(float(redbuffer[flag] - redbuffer[flag-1>=0 ? flag-1 : buffer_length-1])*3);
+    //      Serial.print(",");
+    Serial.print(irbuffer_d[flag]);
+    Serial.print(",");
+    ////      // read stored red
+    Serial.print(redbuffer_d[flag]);
+    Serial.print(",");
+
+    //波峰
+    if (irbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] >= 0 and irbuffer_d[flag] < 0 and (irbuffer_d[flag - 4 >= 0 ? flag - 4 : buffer_length - 4] >= ir_diff_treth * 0.7))
+    {
+      Serial.print(140);
+      ir_diff_treth += irbuffer_d[flag - 4 >= 0 ? flag - 4 : buffer_length - 4];
+      ir_diff_treth /= 2;
+      top_temp = time_t1;
+      
+    }
+    else
+    {
+      Serial.print(0);
+    }
+    Serial.print(",");
+    if (redbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] >= 0 and redbuffer_d[flag] < 0 and (redbuffer_d[flag - 4 >= 0 ? flag - 4 : buffer_length - 4] >= red_diff_treth * 0.7))
+    {
+      Serial.print(-140);
+      red_diff_treth += redbuffer_d[flag - 4 >= 0 ? flag - 4 : buffer_length - 4];
+      red_diff_treth /= 2;
+    }
+    else
+    {
+      Serial.print(0);
+    }
+    Serial.print(",");
+    // 波谷
+    if (time_t1 - top_temp > heart_t_treth * 0.8)
+    {
+      if (irbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] <= 0 and irbuffer_d[flag] > 0 and (irbuffer_d[flag - 4 >= 0 ? flag - 4 : buffer_length - 4] <= -1))
       {
         Serial.print(140);
-        ir_diff_treth += irbuffer_d[flag-4>=0 ? flag-4 : buffer_length-4];
-        ir_diff_treth /= 2;
-        top_temp = time_t1;
-        
+
       }
       else
       {
         Serial.print(0);
       }
       Serial.print(",");
-      if(redbuffer_d[flag-1>=0 ? flag-1 : buffer_length-1] >= 0 and redbuffer_d[flag] < 0 and (redbuffer_d[flag-4>=0 ? flag-4 : buffer_length-4] >= red_diff_treth*0.7))
-      {
-        Serial.print(-140);
-        red_diff_treth += redbuffer_d[flag-4>=0 ? flag-4 : buffer_length-4];
-        red_diff_treth /= 2;
-      }
-      else
-      {
-        Serial.print(0);
-      }
-      Serial.print(",");
-// 波谷
-      if(irbuffer_d[flag-1 >= 0 ? flag-1 : buffer_length-1] <= 0 and irbuffer_d[flag] > 0 and (irbuffer_d[flag-4>=0 ? flag-4 : buffer_length-4] <= -1) and time_t1 - top_temp > heart_t_treth*0.8)
-      {
-        Serial.print(140);
-        
-      }
-      else
-      {
-        Serial.print(0);
-      }
-      Serial.print(",");
-      if(redbuffer_d[flag-1 >= 0 ? flag-1 : buffer_length-1] <= 0 and redbuffer_d[flag] > 0 and (redbuffer_d[flag-4>=0 ? flag-4 : buffer_length-4] <= -1) and time_t1 - top_temp > heart_t_treth*0.8)
+      if (redbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] <= 0 and redbuffer_d[flag] > 0 and (redbuffer_d[flag - 4 >= 0 ? flag - 4 : buffer_length - 4] <= -1))
       {
         Serial.print(-140);
         heart_t_treth += time_t1 - top_temp;
         top_temp = time_t1;
         heart_t_treth /= 2;
-        if(heart_t_treth>50)
+        if (heart_t_treth > 50)
         {
           heart_t_treth = 30;
         }
@@ -141,11 +192,27 @@ void loop() {
       {
         Serial.print(0);
       }
-      // read next set of samples
-//      particleSensor.nextSample();   
-      Serial.print(",");
-//反射波
-      if(irbuffer_d[flag-1>=0 ? flag-1 : buffer_length-1] >= 0 and irbuffer_d[flag] < 0 and time_t1 - top_temp <= heart_t_treth*0.7 and time_t1 - top_temp > heart_t_treth*0.2)
+    }
+    else
+    {
+
+      Serial.print("0,0");
+    }
+
+    // read next set of samples
+    //      particleSensor.nextSample();
+    Serial.print(",");
+    //反射波 明顯
+
+    if (time_t1 - top_temp <= heart_t_treth * 0.7 and time_t1 - top_temp > heart_t_treth * 0.2)
+    {
+      if (irbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] >= 0 and irbuffer_d[flag] < 0 )
+      {
+        Serial.print(140);
+      }//因為記憶體空間不夠只能用這樣
+      else if (irbuffer_d[flag] < 0 and irbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] < 0 \
+               and irbuffer_d[flag - 2 >= 0 ? flag - 2 : buffer_length - 2] < 0  and irbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] > irbuffer_d[flag]\
+               and irbuffer_d[flag - 2 >= 0 ? flag - 2 : buffer_length - 2] < irbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1])
       {
         Serial.print(140);
       }
@@ -154,7 +221,13 @@ void loop() {
         Serial.print(0);
       }
       Serial.print(",");
-      if(redbuffer_d[flag-1>=0 ? flag-1 : buffer_length-1] >= 0 and redbuffer_d[flag] < 0 and time_t1 - top_temp <= heart_t_treth*0.7 and time_t1 - top_temp > heart_t_treth*0.2)
+      if (redbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] >= 0 and redbuffer_d[flag] < 0)
+      {
+        Serial.println(-140);
+      }
+      else if (redbuffer_d[flag] < 0 and redbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] < 0 \
+               and redbuffer_d[flag - 2 >= 0 ? flag - 2 : buffer_length - 2] < 0  and redbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1] > redbuffer_d[flag]\
+               and redbuffer_d[flag - 2 >= 0 ? flag - 2 : buffer_length - 2] < redbuffer_d[flag - 1 >= 0 ? flag - 1 : buffer_length - 1])
       {
         Serial.println(-140);
       }
@@ -162,11 +235,16 @@ void loop() {
       {
         Serial.println(0);
       }
-      time_t1++;
-      flag++;
-      if(flag >= buffer_length)
-      {
-        flag = 0;
-      }
+    }
+    else
+    {
+      Serial.println("0,0");
+    }
+    time_t1++;
+    flag++;
+    if (flag >= buffer_length)
+    {
+      flag = 0;
+    }
   }
 }
